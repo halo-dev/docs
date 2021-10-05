c76f2def87969a3ff280b293c3f30940f9fcd3ee
Ryan Wang <i@ryanc.cc>
2021-10-05 14:35:21 +0800

    refactor: disable blog.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -32,12 +32,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
             showLastUpdateTime: true,
             showLastUpdateAuthor: true
           },
-          blog: {
-            showReadingTime: true,
-            // Please change this to your repo.
-            editUrl:
-              "https://github.com/facebook/docusaurus/edit/main/website/blog/",
-          },
+          blog: false,
           theme: {
             customCss: require.resolve("./src/css/custom.css"),
           },
