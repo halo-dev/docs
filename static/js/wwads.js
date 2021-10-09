039e47b764f2f22cdf63a65a4ec37b303774d58b
Ryan Wang <i@ryanc.cc>
2021-10-10 00:20:56 +0800

    feat: add wwads.

--- static/js/wwads.js
+++ static/js/wwads.js
@@ -3,5 +3,6 @@ window.onload = function () {
   const adnode = document.createElement("div");
   adnode.setAttribute("class", "wwads-cn wwads-vertical");
   adnode.setAttribute("data-id", "80");
+  adnode.setAttribute('style',"max-width:200px")
   ad[0].parentNode.appendChild(adnode);
 };
