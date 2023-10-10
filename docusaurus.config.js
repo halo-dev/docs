ba0c62ef6ba415b8b1824bf73cf99ea7705deede
Ryan Wang <i@ryanc.cc>
2023-10-10 12:40:27 +0800

    docs: update documentation for Halo 2.10 (#262)
    为 Halo 2.10.0 更新文档。
    
    /kind documentation
    
    ```release-note
    None 
    ```

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -34,11 +34,11 @@ const config = {
           showLastUpdateAuthor: true,
           remarkPlugins: [math, mermaid],
           rehypePlugins: [katex],
-          lastVersion: "2.9",
+          lastVersion: "2.10",
           versions: {
             current: {
-              label: "2.10.0-SNAPSHOT",
-              path: "2.10.0-SNAPSHOT",
+              label: "2.11.0-SNAPSHOT",
+              path: "2.11.0-SNAPSHOT",
             },
           },
         },
@@ -284,18 +284,19 @@ const config = {
           if (existingPath.startsWith("/1.4/")) {
             return [existingPath.replace("/1.4/", "/1.4.17/")];
           }
-          if (existingPath.startsWith("/2.10.0-SNAPSHOT/")) {
+          if (existingPath.startsWith("/2.11.0-SNAPSHOT/")) {
             return [
-              existingPath.replace("/2.10.0-SNAPSHOT/", "/2.0.0-SNAPSHOT/"),
-              existingPath.replace("/2.10.0-SNAPSHOT/", "/2.1.0-SNAPSHOT/"),
-              existingPath.replace("/2.10.0-SNAPSHOT/", "/2.2.0-SNAPSHOT/"),
-              existingPath.replace("/2.10.0-SNAPSHOT/", "/2.3.0-SNAPSHOT/"),
-              existingPath.replace("/2.10.0-SNAPSHOT/", "/2.4.0-SNAPSHOT/"),
-              existingPath.replace("/2.10.0-SNAPSHOT/", "/2.5.0-SNAPSHOT/"),
-              existingPath.replace("/2.10.0-SNAPSHOT/", "/2.6.0-SNAPSHOT/"),
-              existingPath.replace("/2.10.0-SNAPSHOT/", "/2.7.0-SNAPSHOT/"),
-              existingPath.replace("/2.10.0-SNAPSHOT/", "/2.8.0-SNAPSHOT/"),
-              existingPath.replace("/2.10.0-SNAPSHOT/", "/2.9.0-SNAPSHOT/"),
+              existingPath.replace("/2.11.0-SNAPSHOT/", "/2.0.0-SNAPSHOT/"),
+              existingPath.replace("/2.11.0-SNAPSHOT/", "/2.1.0-SNAPSHOT/"),
+              existingPath.replace("/2.11.0-SNAPSHOT/", "/2.2.0-SNAPSHOT/"),
+              existingPath.replace("/2.11.0-SNAPSHOT/", "/2.3.0-SNAPSHOT/"),
+              existingPath.replace("/2.11.0-SNAPSHOT/", "/2.4.0-SNAPSHOT/"),
+              existingPath.replace("/2.11.0-SNAPSHOT/", "/2.5.0-SNAPSHOT/"),
+              existingPath.replace("/2.11.0-SNAPSHOT/", "/2.6.0-SNAPSHOT/"),
+              existingPath.replace("/2.11.0-SNAPSHOT/", "/2.7.0-SNAPSHOT/"),
+              existingPath.replace("/2.11.0-SNAPSHOT/", "/2.8.0-SNAPSHOT/"),
+              existingPath.replace("/2.11.0-SNAPSHOT/", "/2.9.0-SNAPSHOT/"),
+              existingPath.replace("/2.11.0-SNAPSHOT/", "/2.10.0-SNAPSHOT/"),
             ];
           }
           return undefined;
