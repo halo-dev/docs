2f9276750e06ca298062b0cbc63859fab2309799
Ryan Wang <i@ryanc.cc>
2024-05-28 00:12:12 +0800

    Fix build
    Signed-off-by: Ryan Wang <i@ryanc.cc>

--- src/theme/SearchBar.js
+++ src/theme/SearchBar.js
@@ -1,22 +1,24 @@
 import React, { useEffect } from "react";
-import SearchBar from "@theme-original/SearchBar";
-import { docsearch } from "meilisearch-docsearch";
 import "meilisearch-docsearch/css";
 
 export default function SearchBarWrapper(props) {
   useEffect(() => {
-    docsearch({
+    const docsearch = require("meilisearch-docsearch").default;
+
+    const destroy = docsearch({
       container: "#docsearch",
       host: "https://docsearch.halo.run",
       // Default Search API Key
       // Use it to search from the frontend
-      apiKey: "4b16205faa360eaa1ee5add67c0d265a4ca1e898ffbea199fe23d487a24c9bc8",
+      apiKey:
+        "4b16205faa360eaa1ee5add67c0d265a4ca1e898ffbea199fe23d487a24c9bc8",
       indexUid: "docs",
     });
+
+    return () => destroy();
   }, []);
   return (
     <>
-      <SearchBar {...props} />
       <div id="docsearch"></div>
     </>
   );
