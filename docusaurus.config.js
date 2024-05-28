15dcd64ed6ab681a46426042706154d17523c172
Ryan Wang <i@ryanc.cc>
2024-05-28 11:34:58 +0800

    Fix ci
    Signed-off-by: Ryan Wang <i@ryanc.cc>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -254,6 +254,16 @@ const config = {
       },
     ],
   ],
+  webpack: {
+    jsLoader: (isServer) => ({
+      loader: require.resolve('esbuild-loader'),
+      options: {
+        loader: 'tsx',
+        format: isServer ? 'cjs' : undefined,
+        target: isServer ? 'node12' : 'es2017',
+      },
+    }),
+  },
   scripts: [
     {
       src: "https://analytics.halo.run/script.js",
